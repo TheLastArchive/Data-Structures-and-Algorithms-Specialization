6eafabbfc9977f854445a4b25b5b938ef57417e7
TheLastArchive <iarchive@hotmail.com>
2022-08-31 15:41:49 +0100

    Completed assignment

--- DataStructures/Week 1/4-stack_with_max_naive.py
+++ DataStructures/Week 1/4-stack_with_max_naive.py
@@ -1,7 +1,7 @@
 #python3
 import sys
 
-class StackWithMax():
+class GivenSolution():
     def __init__(self):
         self.__stack = []
 
@@ -17,12 +17,36 @@ class StackWithMax():
         return max(self.__stack)
 
 
+class StackWithMax():
+    def __init__(self):
+        self.__stack = []
+        self.__max = 0
+
+    def Push(self, a):
+        self.__stack.append(a)
+        if a > self.__max:
+            self.__max = a
+
+    def Pop(self):
+        assert(len(self.__stack))
+        val = self.__stack.pop()
+        if val == self.__max:
+            self.__max = max(self.__stack)
+
+    def Max(self):
+        assert(len(self.__stack))
+        return self.__max
+
 if __name__ == '__main__':
     stack = StackWithMax()
 
-    num_queries = int(sys.stdin.readline())
-    for _ in range(num_queries):
-        query = sys.stdin.readline().split()
+    num_queries = int(input("Num queries: "))
+    for i in range(num_queries):
+        query = input(f"Query {i + 1}: ").split()
+        
+    # num_queries = int(sys.stdin.readline())
+    # for _ in range(num_queries):
+    #     query = sys.stdin.readline().split()
 
         if query[0] == "push":
             stack.Push(int(query[1]))
